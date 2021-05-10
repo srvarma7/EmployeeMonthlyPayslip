1a12a8b157252e66b0269367453b453c5fc31221
srvarma7 <48859937+srvarma7@users.noreply.github.com>
2021-05-10 22:53:18 +1000

    Created data models for Employee and Income

---
+++ EmployeeMonthlyPayslip/Models/Employee.swift
@@ -0,0 +1,11 @@
+//
+//  Employee.swift
+//  EmployeeMonthlyPayslip
+//
+//  Created by Sai Raghu Varma Kallepalli on 10/5/21.
+//
+
+struct Employee {
+    let name: String
+    let annualSalary: Double
+}
---
+++ EmployeeMonthlyPayslip/Models/Income.swift
@@ -0,0 +1,13 @@
+//
+//  Income.swift
+//  EmployeeMonthlyPayslip
+//
+//  Created by Sai Raghu Varma Kallepalli on 10/5/21.
+//
+
+struct Income {
+    let employeeName: String
+    let grossMonthlyIncome: Double
+    let monthlyIncomeTax: Double
+    let netMonthlyIncome: Double
+}
