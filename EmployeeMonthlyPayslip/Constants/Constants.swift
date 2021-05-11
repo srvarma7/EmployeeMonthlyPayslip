98dcadd4b905cb592f1e1570f283770dde4bb6d4
srvarma7 <48859937+srvarma7@users.noreply.github.com>
2021-05-11 18:00:36 +1000

    Updated Constants to add Tax constants

--- EmployeeMonthlyPayslip/Constants/Constants.swift
+++ EmployeeMonthlyPayslip/Constants/Constants.swift
@@ -13,4 +13,8 @@ struct Constants {
         static let MAX_VALID_NAME_LENGTH: Int   = 37
         static let VALID_NAME_REGEX: String     = "^[a-zA-Z ]{\(MIN_VALID_NAME_LENGTH),\(MAX_VALID_NAME_LENGTH)}$"
     }
+    struct Tax {
+        static let RATES: [Double]       = [0, 0.1, 0.2, 0.3, 0.4]
+        static let BASE_VALUES: [Double] = [0, 20000, 40000, 80000, 180000]
+    }
 }
