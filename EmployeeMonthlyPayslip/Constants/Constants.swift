11e3013842b809e146c8204543166d39133e2585
srvarma7 <48859937+srvarma7@users.noreply.github.com>
2021-05-12 01:58:27 +1000

    Updated constants and changed structure

--- EmployeeMonthlyPayslip/Constants/Constants.swift
+++ EmployeeMonthlyPayslip/Constants/Constants.swift
@@ -5,16 +5,21 @@
 //  Created by Sai Raghu Varma Kallepalli on 10/5/21.
 //
 
+/**
+ Constants for defining the required values
+ */
 struct Constants {
-    struct General {
-        static let MONTHS_IN_A_Year: Int        = 12
+    // Constants related to general Validation
+    struct Validation {
         static let KEYWORD: String              = "GenerateMonthlyPayslip"
         static let MIN_VALID_NAME_LENGTH: Int   = 2
         static let MAX_VALID_NAME_LENGTH: Int   = 37
         static let VALID_NAME_REGEX: String     = "^[a-zA-Z ]{\(MIN_VALID_NAME_LENGTH),\(MAX_VALID_NAME_LENGTH)}$"
     }
+    // Constants related to Tax calculation
     struct Tax {
-        static let RATES: [Double]       = [0, 0.1, 0.2, 0.3, 0.4]
-        static let BASE_VALUES: [Double] = [0, 20000, 40000, 80000, 180000]
+        static let MONTHS_IN_A_Year: Int        = 12
+        static let RATE_PERCENTAGE: [Double]    = [0, 0.1, 0.2, 0.3, 0.4]
+        static let BASE_VALUES_SLAB: [Double]   = [0, 20000, 40000, 80000, 180000]
     }
 }
